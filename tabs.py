2510b248b8ed1a545bd779c0db6405fd439a518e
David J Harcourt <davidjharcourt@gmail.com>
2019-01-20 17:30:44 -0500

    First commit

---
+++ tabs.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+for i in range (1,12):
+    print("No {0:4} squared is {1:4} and cubed is {2:5}".format(i,i**2,i**4))
+    
+    
